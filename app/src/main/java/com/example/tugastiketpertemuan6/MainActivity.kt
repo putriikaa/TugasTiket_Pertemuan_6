229f105ad2023f1e882e1593cf598caccc43b7b7
putriikaa <anandakusuma36@gmail.com>
2023-09-24 18:55:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugastiketpertemuan6/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.tugastiketpertemuan6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Toast
+import com.example.tugastiketpertemuan6.R
+import com.example.tugastiketpertemuan6.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val tiket = arrayOf(
+            "First Class",
+            "Business Class",
+            "Economy Class"
+        )
+
+        with(binding){
+            val jenistiketAdapter = ArrayAdapter(this@MainActivity,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,tiket)
+
+            jenistiketAdapter.setDropDownViewResource(
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
+
+            spinJenisTiket.adapter= jenistiketAdapter
+
+            var selectedDate = ""
+            var selectedTime = ""
+
+            datePick.init(datePick.year, datePick.month, datePick.dayOfMonth) { _, year, month, day ->
+                selectedDate = "$day/${month + 1}/$year"
+            }
+
+            timePicker.setOnTimeChangedListener { _, hourOfDay, minutes ->
+                selectedTime = String.format("%02d:%02d", hourOfDay, minutes)
+            }
+
+            val pesanButton = findViewById<Button>(R.id.button_pesan)
+
+            pesanButton.setOnClickListener {
+                val toastMessage =
+                    "Tiket berhasil dipesan pada tanggal $selectedDate jam $selectedTime"
+                Toast.makeText(this@MainActivity, toastMessage, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        }
+    }
